7f723e785ff19178ce6f1e2a7e39abcc5109fe2e
ChristophHu <christoph.huschenhoefer@gmx.de>
2019-12-09 18:38:09 +0100

    sidebar updated

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core'
 
+// Interface
+export interface RouteInfo {
+    path: string
+    title: string
+    type: string
+    icontype: string
+}
+
+// Routes
+export const ROUTES: RouteInfo[] = [
+    {
+        path: '/dashboard',
+        title: 'Dashboard',
+        type: 'link',
+        icontype: 'fas fa-chart-line'
+    },
+    {
+        path: '/project',
+        title: 'Projekte',
+        type: 'link',
+        icontype: 'fas fa-project-diagram'
+    }
+]
 @Component({
   selector: 'app-sidebar',
   templateUrl: 'sidebar.component.html',
   styles: []
 })
 export class SidebarComponent implements OnInit {
+    public menuItems: any[]
 
     constructor() { }
 
     ngOnInit() {
-
+        this.menuItems = ROUTES.filter(menuItem => menuItem)
     }
 }
